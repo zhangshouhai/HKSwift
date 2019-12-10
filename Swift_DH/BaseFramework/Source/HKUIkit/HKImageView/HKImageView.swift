5e5b57b3b604805c5caa7c34279d2f4b15d84876
张寿海 <zhangshouhaivip@sina.cn>
2019-12-10 08:47:03 +0800

    HKImageView 创建圆角

---
+++ Swift_DH/BaseFramework/Source/HKUIkit/HKImageView/HKImageView.swift
@@ -0,0 +1,19 @@
+//
+//  HKImageView.swift
+//  BaseFramework
+//
+//  Created by mac on 2019/12/10.
+//  Copyright © 2019 贾则栋. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+func HKImageView(radius:CGFloat,isRadius:Bool) -> UIImageView{
+    let imageview = UIImageView()
+    imageview.layer.cornerRadius = radius
+    imageview.layer.masksToBounds = isRadius
+    imageview.contentMode = .scaleAspectFill
+    return imageview;
+}
