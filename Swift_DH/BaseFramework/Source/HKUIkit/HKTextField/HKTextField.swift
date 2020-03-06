39a06584dd376f16b4b86a145e952fcb743f2119
CaoLuokai <815108092@qq.com>
2020-03-06 13:27:42 +0800

    更新HKTextField

--- Swift_DH/BaseFramework/Source/HKUIkit/HKTextField/HKTextField.swift
+++ Swift_DH/BaseFramework/Source/HKUIkit/HKTextField/HKTextField.swift
@@ -11,9 +11,12 @@ import Foundation
 
 class HKTextField: HKBaseView,UITextFieldDelegate {
     
+    open var textfield = UITextField()
+    open var rightImgView = UIImageView()
+    
     public func createHKTextField(font:CGFloat,text:String,placeholder:String,keyboardType:UIKeyboardType,leftLabStr:String, leftImgStr:String, rightImgStr:String ,showLine:Bool) -> (){
         
-        let textfield = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        textfield = UITextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         textfield.borderStyle = UITextField.BorderStyle.none
         textfield.placeholder = placeholder
         textfield.adjustsFontSizeToFitWidth = true
@@ -22,35 +25,36 @@ class HKTextField: HKBaseView,UITextFieldDelegate {
         textfield.clearButtonMode = .whileEditing
         textfield.keyboardType = keyboardType
         textfield.returnKeyType = UIReturnKeyType.done
-        textfield.font = UIFont.systemFont(ofSize: HKFixWidthFlaot(14))
+        textfield.font = UIFont.systemFont(ofSize: font)
+        textfield.text = text
         textfield.delegate = self
         
         self.addSubview(textfield)
         
     
-//        textfield.snp.makeConstraints { (make) in
-//            make.left.equalTo(HKFixWidthFlaot(10))
-//            make.centerY.equalToSuperview()
-//            make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
-//            make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
-//        }
-//
-//        if leftLabStr.count > 0  {
-//            let label = HKLabel(font: fontSize14, color: UIColor.gray, text: leftLabStr)
-//            self.addSubview(label)
-//            let labWidth = AppStyleConfiguration.labelWidth(leftLabStr, CGFloat.greatestFiniteMagnitude)
-//            label.snp.makeConstraints { (make) in
-//                make.left.equalTo(HKFixWidthFlaot(10))
-//                make.centerY.equalToSuperview()
-//                make.width.equalTo(labWidth)
-//            }
-//           textfield.snp_updateConstraints { (make) in
-//                make.left.equalTo(label.snp_right).offset(HKFixWidthFlaot(5))
-//                make.centerY.equalToSuperview()
-//                make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
-//                make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
-//            }
-//        }
+        textfield.snp.makeConstraints { (make) in
+            make.left.equalTo(HKFixWidthFlaot(10))
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
+            make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
+        }
+
+        if leftLabStr.count > 0  {
+            let label = HKLabel(font: fontSize14, color: UIColor.gray, text: leftLabStr)
+            self.addSubview(label)
+            let labWidth = AppStyleConfiguration.labelWidth(leftLabStr, CGFloat.greatestFiniteMagnitude)
+            label.snp.makeConstraints { (make) in
+                make.left.equalTo(HKFixWidthFlaot(10))
+                make.centerY.equalToSuperview()
+                make.width.equalTo(labWidth)
+            }
+           textfield.snp_remakeConstraints { (make) in
+                make.left.equalTo(label.snp_right).offset(HKFixWidthFlaot(5))
+                make.centerY.equalToSuperview()
+                make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
+                make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
+            }
+        }
         
         if leftImgStr.count > 0 {
             let leftImgView = HKImageView(radius: 0.0, isRadius: true)
@@ -60,40 +64,37 @@ class HKTextField: HKBaseView,UITextFieldDelegate {
             self.addSubview(leftImgView)
             
             leftImgView.snp.makeConstraints { (make) in
-                make.left.equalTo(HKFixWidthFlaot(10))
+                make.left.equalTo(HKFixWidthFlaot(0))
                 make.centerY.equalToSuperview()
                 make.width.height.equalTo(30)
             }
             
-            textfield.snp.makeConstraints { (make) in
+            textfield.snp_remakeConstraints { (make) in
                 make.left.equalTo(leftImgView.snp_right).offset(HKFixWidthFlaot(5))
                 make.centerY.equalToSuperview()
                 make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
                 make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
             }
-            
-
-            
         }
         
         if rightImgStr.count > 0 {
-            let rightImgView = HKImageView(radius: 0.0, isRadius: true)
+            rightImgView = HKImageView(radius: 0.0, isRadius: true)
             rightImgView.image = UIImage.init(named: rightImgStr)
             rightImgView.width = (rightImgView.image?.size.width)!+20
             rightImgView.contentMode = UIView.ContentMode.center
             textfield.rightView = rightImgView
             
-//           textfield.snp_updateConstraints { (make) in
-//                make.left.equalToSuperview().offset(HKFixWidthFlaot(10))
-//                make.centerY.equalToSuperview()
-//                make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
-//                make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
-//            }
+           textfield.snp_remakeConstraints { (make) in
+                make.left.equalToSuperview().offset(HKFixWidthFlaot(10))
+                make.centerY.equalToSuperview()
+                make.right.equalToSuperview().offset(-HKFixWidthFlaot(10))
+                make.top.equalToSuperview().offset(HKFixHeightFlaot(5))
+            }
         }
         
         if showLine {
             let lineView = HKBaseView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-            lineView.backgroundColor = UIColor.gray
+            lineView.backgroundColor = UIColor.colorWithHexString(colorString: "#D5D5D5")
             self.addSubview(lineView)
             lineView.snp.makeConstraints { (make) in
                 make.left.right.bottom.equalToSuperview()
