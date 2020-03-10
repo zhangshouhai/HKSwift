85ad034fea53a54a1eb89b8bb979f76448a0d66f
CaoLuokai <815108092@qq.com>
2020-03-10 16:42:03 +0800

    更新json处理

---
+++ Swift_DH/Pods/KakaJSON/Sources/KakaJSON/Extension/Array+KJ.swift
@@ -0,0 +1,58 @@
+//
+//  Array+KJ.swift
+//  KakaJSON
+//
+//  Created by MJ Lee on 2019/8/12.
+//  Copyright © 2019 MJ Lee. All rights reserved.
+//
+
+import Foundation
+
+extension NSArray: KJCompatible {}
+extension Array: KJCompatible {}
+extension Set: KJCompatible {}
+extension NSSet: KJCompatible {}
+
+public extension KJ where Base: ExpressibleByArrayLiteral & Sequence {
+    /// JSONObjectArray -> ModelArray
+    func modelArray<M: Convertible>(_ type: M.Type) -> [M] {
+        return modelArray(type: type) as! [M]
+    }
+    
+    /// JSONObjectArray -> ModelArray
+    func modelArray(type: Convertible.Type) -> [Convertible] {
+        guard let mt = Metadata.type(type) as? ModelType,
+            let _ = mt.properties
+            else { return [] }
+        return base.compactMap {
+            switch $0 {
+            case let dict as [String: Any]: return dict.kj_fastModel(type)
+            case let dict as Data: return dict.kj_fastModel(type)
+            case let dict as String: return dict.kj_fastModel(type)
+            default: return nil
+            }
+        }
+    }
+    
+    /// ModelArray -> JSONObjectArray
+    func JSONObjectArray() -> [[String: Any]] {
+        return base.compactMap {
+            ($0~! as? Convertible)?.kj_JSONObject()
+        }
+    }
+    
+    /// Array -> JSONArray
+    func JSONArray() -> [Any] {
+        return Values.JSONValue(base) as! [Any]
+    }
+    
+    /// Array -> JSONString
+    func JSONString(prettyPrinted: Bool = false) -> String {
+        if let str = JSONSerialization.kj_string(JSONArray(),
+                                                 prettyPrinted: prettyPrinted) {
+            return str
+        }
+        Logger.error("Failed to get JSONString from JSON.")
+        return ""
+    }
+}
---
+++ Swift_DH/Pods/KakaJSON/Sources/KakaJSON/Metadata/Type/ModelType.swift
@@ -0,0 +1,40 @@
+//
+//  ModelType.swift
+//  KakaJSON
+//
+//  Created by MJ Lee on 2019/8/13.
+//  Copyright © 2019 MJ Lee. All rights reserved.
+//
+
+import Foundation
+
+public class ModelType: BaseType {
+    public internal(set) var properties: [Property]?
+    public internal(set) var genericTypes: [Any.Type]?
+    private var modelKeysLock = DispatchSemaphore(value: 1)
+    private var modelKeys: [String: ModelPropertyKey] = [:]
+    private var jsonKeysLock = DispatchSemaphore(value: 1)
+    private var jsonKeys: [String: String] = [:]
+    
+    func modelKey(from propertyName: String,
+                  _ createdKey: @autoclosure () -> ModelPropertyKey) -> ModelPropertyKey {
+        modelKeysLock.wait()
+        defer { modelKeysLock.signal() }
+        if let key = modelKeys[propertyName] { return key }
+
+        let resultKey = createdKey()
+        modelKeys[propertyName] = resultKey
+        return resultKey
+    }
+    
+    func JSONKey(from propertyName: String,
+                 _ createdKey: @autoclosure () -> String) -> String {
+        jsonKeysLock.wait()
+        defer { jsonKeysLock.signal() }
+        if let key = jsonKeys[propertyName] { return key }
+        
+        let resultKey = createdKey()
+        jsonKeys[propertyName] = resultKey
+        return resultKey
+    }
+}
